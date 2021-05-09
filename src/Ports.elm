fee7e0ff55ef5a9033310fa055464e88bac3ddca
Jaime Terreu <jaime@terreu.com>
2021-05-09 21:56:38 +0930

    Elm packages can't dispatch ports
    Will need to do something more clever to auto size the input.

--- src/Ports.elm
+++
@@ -1,9 +0,0 @@
-port module Ports exposing (kaizenConnectSelectInputDynamicWidth, kaizenDisconnectSelectInputDynamicWidth)
-
-import Json.Encode as Encode
-
-
-port kaizenConnectSelectInputDynamicWidth : Encode.Value -> Cmd msg
-
-
-port kaizenDisconnectSelectInputDynamicWidth : Encode.Value -> Cmd msg
