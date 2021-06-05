8006d400da1f27575aa9ae9ec029df1c9c376325
Jaime Terreu <jaime@terreu.com>
2021-06-05 17:46:13 +0930

    Add playwright tests
    Started off with a really simple test to check that the elm examples
    exist.

--- tests/test.ts
+++
@@ -1,8 +0,0 @@
-import { chromium } from "playwright";
-
-(async () => {
-  const browser = await chromium.launch({ headless: false });
-  const page = await browser.newPage();
-  await page.goto("http://localhost:8000");
-  await browser.close();
-})();
