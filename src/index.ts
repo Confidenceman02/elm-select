9272fe47396518c38f71a79fad0c5d08040f11bb
Jaime Terreu <jaime.terreu@cultureamp.com>
2021-07-07 16:56:08 +0930

    Run ci script in predeploy

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import defo from "@icelab/defo";
 /**
  * Set defo prefix to `ex`. This means we call it in the DOM with the convention:
  *
- *    data-ex-#{view-observer-name}={...}
+ *    data-es-#{view-observer-name}={...}
  */
-const prefix = "ex";
+const prefix = "es";
 
 defo({ prefix, views: {} });
