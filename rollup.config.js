ccf8755432f1c5c7b4d90c50c09fad85279b9d27
Jaime Terreu <jaime@terreu.com>
2021-07-18 20:10:21 +0930

    Ammend rollup.config file
    This change adds different targets

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,19 @@
 import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
 import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "src/index.ts",
-  output: {
-    file: "dist/dynamic.min.js",
-    format: "umd",
-    globals: { "@icelab/defo": "defo" },
-  },
+  output: [
+    { file: pkg.main, format: "cjs" },
+    { file: pkg.module, format: "es" },
+    { file: pkg.browser, format: "umd", name: "ElmSelect" },
+    {
+      file: "dist/dynamic.min.js",
+      format: "umd",
+      globals: { "@icelab/defo": "defo" },
+    },
+  ],
   plugins: [uglify(), typescript({ typescript: require("typescript") })],
   external: ["@icelab/defo"],
 };
