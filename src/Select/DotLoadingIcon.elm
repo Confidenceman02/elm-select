6c0c1c73cead853ae1c9ec93348e94302495fa28
Jaime Terreu <jaime@terreu.com>
2021-12-28 00:17:55 +1030

    feat: add animation-play-state property

--- src/Select/DotLoadingIcon.elm
+++ src/Select/DotLoadingIcon.elm
@@ -22,8 +22,9 @@ view =
                         -- , ( 100, [ CssAnimation.opacity (Css.num 0) ] )
                         ]
                 , Css.animationDuration (Css.sec logoAnimationDuration)
-                , Css.property "animation-iteration-count" "infinite"
+                , Css.animationIterationCount Css.infinite
                 , Css.animationDelay (Css.sec (logoAnimationDelay * 2))
+                , Css.property "animation-play-state" "running"
                 ]
             , d "M30.8284 1.17157C32.3905 2.73366 32.3905 5.26633 30.8284 6.82842C29.2663 8.39051 26.7336 8.39051 25.1715 6.82842C23.6094 5.26633 23.6094 2.73366 25.1715 1.17157C26.7336 -0.390523 29.2663 -0.390523 30.8284 1.17157Z"
             ]
@@ -40,8 +41,9 @@ view =
                         -- , ( 100, [ CssAnimation.opacity (Css.num 0) ] )
                         ]
                 , Css.animationDuration (Css.sec logoAnimationDuration)
-                , Css.property "animation-iteration-count" "infinite"
+                , Css.animationIterationCount Css.infinite
                 , Css.animationDelay (Css.sec logoAnimationDelay)
+                , Css.property "animation-play-state" "running"
                 ]
             , d "M18.8285 1.17157C20.3906 2.73366 20.3906 5.26633 18.8285 6.82842C17.2664 8.39051 14.7337 8.39051 13.1716 6.82842C11.6095 5.26633 11.6095 2.73366 13.1716 1.17157C14.7337 -0.390523 17.2664 -0.390523 18.8285 1.17157Z"
             ]
@@ -58,7 +60,8 @@ view =
                         -- , ( 100, [ CssAnimation.opacity (Css.num 0) ] )
                         ]
                 , Css.animationDuration (Css.sec logoAnimationDuration)
-                , Css.property "animation-iteration-count" "infinite"
+                , Css.animationIterationCount Css.infinite
+                , Css.property "animation-play-state" "running"
                 ]
             , d "M6.82848 1.17157C8.39057 2.73366 8.39057 5.26633 6.82848 6.82842C5.26639 8.39051 2.73372 8.39051 1.17163 6.82842C-0.390462 5.26633 -0.390462 2.73366 1.17163 1.17157C2.73372 -0.390523 5.26639 -0.390523 6.82848 1.17157Z"
             ]
