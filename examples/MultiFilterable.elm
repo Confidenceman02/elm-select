f0e6b20cd709e432d543140aa692f71b23fd6bfc
Jaime Terreu <jaime@Jaimes-MacBook-Pro.local>
2022-08-07 10:06:47 +0930

    feat: Add killer filterable example

--- examples/MultiFilterable.elm
+++ examples/MultiFilterable.elm
@@ -2,19 +2,26 @@ module MultiFilterable exposing (..)
 
 import Browser
 import Css
-import Html.Styled as Styled exposing (Html, div)
+import Html.Styled as Styled exposing (Html, div, text)
 import Html.Styled.Attributes as StyledAttribs
+import List.Extra as LE
 import Select exposing (MenuItem, initState, selectIdentifier, update)
 
 
 type Msg
-    = SelectMsg (Select.Msg String)
+    = SelectMsg (Select.Msg Item)
+
+
+type Item
+    = Item String
+    | NewItem String
+    | ItemBuilder String
 
 
 type alias Model =
     { selectState : Select.State
-    , items : List (MenuItem String)
-    , selectedItems : List String
+    , items : List Item
+    , selectedItems : List Item
     }
 
 
@@ -22,10 +29,11 @@ init : ( Model, Cmd Msg )
 init =
     ( { selectState = initState
       , items =
-            [ Select.basicMenuItem { item = "Elm", label = "Elm" }
-            , Select.basicMenuItem { item = "Is", label = "Is" }
-            , Select.basicMenuItem { item = "Really", label = "Really" }
-            , Select.basicMenuItem { item = "NonFilteralbe", label = "Non Filterable" } |> Select.filterableMenuItem False
+            [ Item "Elm"
+            , Item "Is"
+            , Item "Really"
+            , Item "Great"
+            , ItemBuilder ""
             ]
       , selectedItems = []
       }
@@ -51,25 +59,130 @@ update msg model =
                 ( maybeAction, selectState, cmds ) =
                     Select.update sm model.selectState
 
-                updatedSelectedItems =
+                updatedModel =
                     case maybeAction of
+                        Just (Select.InputChange query) ->
+                            { model | items = LE.updateIf isItemBuilder (\_ -> ItemBuilder query) model.items }
+
                         Just (Select.Select i) ->
-                            model.selectedItems ++ [ i ]
+                            case i of
+                                ItemBuilder val ->
+                                    let
+                                        newItem =
+                                            NewItem val
+                                    in
+                                    { model
+                                        | selectedItems = model.selectedItems ++ [ newItem ]
+                                        , items = LE.updateIf isItemBuilder (\_ -> newItem) model.items ++ [ ItemBuilder "" ]
+                                    }
+
+                                _ ->
+                                    { model | selectedItems = model.selectedItems ++ [ i ] }
 
                         Just (Select.DeselectMulti item) ->
-                            List.filter (\i -> item /= i) model.selectedItems
+                            { model | selectedItems = List.filter (\i -> item /= i) model.selectedItems }
 
                         _ ->
-                            model.selectedItems
+                            model
             in
-            ( { model | selectState = selectState, selectedItems = updatedSelectedItems }, Cmd.map SelectMsg cmds )
+            ( { updatedModel | selectState = selectState }, Cmd.map SelectMsg cmds )
+
+
+isItemBuilder : Item -> Bool
+isItemBuilder i =
+    case i of
+        ItemBuilder _ ->
+            True
+
+        _ ->
+            False
+
+
+itemMap : (Item -> Item) -> Item -> Item
+itemMap f =
+    f
+
+
+getItemValue : Item -> String
+getItemValue i =
+    case i of
+        Item val ->
+            val
+
+        NewItem val ->
+            val
+
+        ItemBuilder val ->
+            val
+
+
+itemToMenuItem : Item -> MenuItem Item
+itemToMenuItem i =
+    case i of
+        Item val ->
+            Select.basicMenuItem { item = i, label = val }
+
+        NewItem val ->
+            Select.basicMenuItem { item = i, label = val }
+
+        ItemBuilder val ->
+            Select.customMenuItem
+                { item = i
+                , label = val
+                , view =
+                    let
+                        resolveValue =
+                            if String.isEmpty val then
+                                "Type text to add new value"
+
+                            else
+                                val
+                    in
+                    div
+                        [ StyledAttribs.css
+                            [ Css.displayFlex
+                            , Css.position Css.relative
+                            ]
+                        ]
+                        [ div
+                            [ StyledAttribs.css
+                                [ Css.position Css.absolute
+                                , Css.height (Css.pct 100)
+                                , Css.border3 (Css.px 1) Css.solid (Css.hex "#000000")
+                                ]
+                            ]
+                            []
+                        , div
+                            [ StyledAttribs.css [ Css.marginLeft (Css.rem 1) ]
+                            ]
+                            [ div
+                                [ StyledAttribs.css
+                                    [ Css.fontSize (Css.px 16)
+                                    , Css.color (Css.hex "#1576CF")
+                                    ]
+                                ]
+                                [ text "New value" ]
+                            , div [ StyledAttribs.css [ Css.fontSize (Css.px 14) ] ] [ text resolveValue ]
+                            ]
+                        ]
+                }
+                |> Select.filterableMenuItem False
 
 
 view : Model -> Html Msg
 view m =
     let
-        selectedItems =
-            List.map (\i -> Select.basicMenuItem { item = i, label = i }) m.selectedItems
+        selectedItemToMenuItem : Item -> MenuItem Item
+        selectedItemToMenuItem item =
+            case item of
+                Item val ->
+                    Select.basicMenuItem { item = item, label = val }
+
+                NewItem val ->
+                    Select.basicMenuItem { item = item, label = val }
+
+                ItemBuilder val ->
+                    Select.basicMenuItem { item = item, label = val }
     in
     div
         [ StyledAttribs.css
@@ -81,9 +194,9 @@ view m =
         ]
         [ Styled.map SelectMsg <|
             Select.view
-                (Select.multi selectedItems
+                (Select.multi (List.map selectedItemToMenuItem m.selectedItems)
                     |> Select.state m.selectState
-                    |> Select.menuItems m.items
+                    |> Select.menuItems (List.map itemToMenuItem m.items)
                     |> Select.placeholder "Placeholder"
                 )
                 (selectIdentifier "SingleSelectExample")
