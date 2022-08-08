c9bcf98b516350cfb577cec9f6ee041793552de3
Jaime Terreu <jaime@Jaimes-MBP.lan>
2022-08-08 21:21:32 +0930

    feat: make headless example

---
+++ examples/HeadlessSingle.elm
@@ -0,0 +1,96 @@
+module HeadlessSingle exposing (..)
+
+import Browser
+import Css
+import Html.Styled as Styled exposing (Html, div)
+import Html.Styled.Attributes as StyledAttribs
+import Select exposing (MenuItem, initState, selectIdentifier, update)
+
+
+type Msg
+    = SelectMsg (Select.Msg String)
+
+
+type alias Model =
+    { selectState : Select.State String
+    , items : List (MenuItem String)
+    , selectedItem : Maybe String
+    , selectId : Select.SelectId
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { selectState = initState
+      , items =
+            [ Select.basicMenuItem { item = "Elm", label = "Elm" }
+            , Select.basicMenuItem { item = "Is", label = "Is" }
+            , Select.basicMenuItem { item = "Really", label = "Really" }
+            , Select.basicMenuItem { item = "Great", label = "Great" }
+            ]
+      , selectedItem = Nothing
+      , selectId =
+            selectIdentifier "SingleSelectExample"
+      }
+    , Cmd.none
+    )
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = always init
+        , view = view >> Styled.toUnstyled
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SelectMsg sm ->
+            let
+                ( maybeAction, selectState, cmds ) =
+                    Select.update sm model.selectState
+
+                updatedSelectedItem =
+                    case maybeAction of
+                        Just (Select.Select i) ->
+                            Just i |> Debug.log "Selected"
+
+                        _ ->
+                            model.selectedItem
+            in
+            ( { model | selectState = selectState, selectedItem = updatedSelectedItem }, Cmd.map SelectMsg cmds )
+
+
+view : Model -> Html Msg
+view m =
+    let
+        selectedItem =
+            case m.selectedItem of
+                Just i ->
+                    Just (Select.basicMenuItem { item = i, label = i })
+
+                _ ->
+                    Nothing
+    in
+    div
+        [ StyledAttribs.css
+            [ Css.marginTop (Css.px 20)
+            , Css.width (Css.pct 50)
+            , Css.marginLeft Css.auto
+            , Css.marginRight Css.auto
+            ]
+        ]
+        [ Styled.map SelectMsg <|
+            Select.view
+                (Select.single selectedItem
+                    |> Select.state m.selectState
+                    |> Select.menuItems m.items
+                    |> Select.placeholder "Placeholder"
+                    |> Select.searchable False
+                )
+                m.selectId
+        ]
